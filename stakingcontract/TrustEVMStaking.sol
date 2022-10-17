da133361f08083173c17da36f075341577f2b09f
Daniel Bellone <104389037+danielbellone@users.noreply.github.com>
2022-10-17 11:45:22 -0700

    MultiCall function moved to a separate contract.

--- stakingcontract/TrustEVMStaking.sol
+++ stakingcontract/TrustEVMStaking.sol
@@ -255,19 +255,4 @@ contract TrustEVMStaking is AccessControlEnumerable {
         renounceRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    // MultiCall //
-
-    function multiCall(address[] calldata targets, bytes[] calldata data) external view returns (bytes[] memory) {
-        require(targets.length == data.length, "target length != data length");
-        bytes[] memory results = new bytes[](data.length);
-
-        for (uint i; i < targets.length; i++) {
-            (bool success, bytes memory result) = targets[i].staticcall(data[i]);
-            require(success, 'call failed');
-            results[i] = result;
-        }
-
-        return results;
-    }
-
 }
