5ef46dbb0307ba9b61e473d42d7a56d6137489f7
Zach Butler <zach.butler@eosnetwork.com>
2023-01-18 00:54:16 -0500

    Create CI pipeline to build TrustEVM contract(s)

---
+++ .github/workflows/contract-build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -eo pipefail
+
+# print and run a command
+function ee()
+{
+    echo "$ $*"
+    eval "$@"
+}
+
+# debug code
+ee cdt-cc --version
+ee cmake --version
+
+# build
+ee mkdir -p contract/build
+ee pushd contract
+ee pushd build
+ee cmake -DCMAKE_BUILD_TYPE=Release -DWITH_TEST_ACTIONS=On ..
+ee make -j "$(nproc)"
+
+# pack
+ee popd
+ee 'tar -czf ../build.tar.gz build/*'
+ee popd
+
+echo 'Done! - contract-build.sh'
