31d8648a342fb416cf2c9ec71631bb599a3893e3
Zach Butler <zach.butler@eosnetwork.com>
2023-01-18 01:47:16 -0500

    Comments and whitespace

--- .github/workflows/build-node.sh
+++ .github/workflows/build-node.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 set -eo pipefail
 
+# print and run a command
 function ee()
 {
     echo "$ $*"
     eval "$@"
 }
 
+# debug code
 ee cmake --version
 ee gcc --version
+
+# build
 ee mkdir build
 ee pushd build
 ee cmake ..
 ee make -j "$(nproc)"
+
+# pack
 ee popd
 ee 'tar -czf build.tar.gz build/*'
+
 echo 'Done! - build-node.sh'
