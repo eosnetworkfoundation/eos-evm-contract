2d3ecc9f0c28114476d4844f165f473a3eb61918
Zach Butler <zach.butler@eosnetwork.com>
2023-02-08 01:59:44 -0500

    Specify release build type for contract tests

--- .github/workflows/build-contract-test.sh
+++ .github/workflows/build-contract-test.sh
@@ -18,7 +18,7 @@ cat "$Deosio_DIR/EosioTester.cmake" | grep 'EOSIO_VERSION' | grep -oP "['\"].*['
 ee mkdir -p contract/tests/build
 ee pushd contract/tests
 ee pushd build
-ee "cmake .."
+ee "cmake -DCMAKE_BUILD_TYPE=Release .."
 ee make -j "$(nproc)" unit_test
 
 # pack
