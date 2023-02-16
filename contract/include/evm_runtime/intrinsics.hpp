3634b17e87aa0739f6c4de2e5ad998c399b1c562
Matt Witherspoon <32485495+spoonincode@users.noreply.github.com>
2023-02-16 16:30:05 -0500

    remove unused get_block_num() definition

--- contract/include/evm_runtime/intrinsics.hpp
+++ contract/include/evm_runtime/intrinsics.hpp
@@ -2,9 +2,6 @@
 namespace eosio {
    namespace internal_use_do_not_use {
       extern "C" {
-         __attribute__((eosio_wasm_import))
-         uint32_t get_block_num();
-
         #ifdef WITH_LOGTIME
         __attribute__((eosio_wasm_import))
          void logtime(const char*);
