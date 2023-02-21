a570908d744b37844a2585400028b384a2778319
Zach Butler <zach.butler@eosnetwork.com>
2023-02-21 17:05:14 -0500

    Try directing hunter build to use more cores (or any other cMake dep builds)

--- .github/workflows/build-node.sh
+++ .github/workflows/build-node.sh
@@ -10,6 +10,8 @@ function ee()
 
 # install dependencies for self-hosted runners
 ee export DEBIAN_FRONTEND='noninteractive'
+ee export HUNTER_JOBS_NUMBER="$(nproc)"
+ee export NUMBER_OF_LOGICAL_CORES="$(nproc)"
 ee apt-get update -q
 ee apt-get install -yqq build-essential cmake gcc-10 g++-10 git
 ee git config --global --add safe.directory '*' # silence all safe.directory git warnings inside container (causing "fatal: detected dubious ownership in repository")
@@ -17,6 +19,8 @@ ee git config --global --add safe.directory '*' # silence all safe.directory git
 # debug code
 echo "CC='${CC}'"
 echo "CXX='${CXX}'"
+echo "HUNTER_JOBS_NUMBER='${HUNTER_JOBS_NUMBER}'"
+echo "NUMBER_OF_LOGICAL_CORES='${NUMBER_OF_LOGICAL_CORES}'"
 ee cmake --version
 ee nproc
 ee free -h
