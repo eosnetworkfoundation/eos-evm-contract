8304dab48fffcda2a7b1fb35260d86a49c9c7515
Matias Romeo <matias.romeo@gmail.com>
2023-03-14 00:23:42 -0300

    Make CICD step to fail if there are failures in ctest

--- .github/workflows/test-contract.sh
+++ .github/workflows/test-contract.sh
@@ -9,6 +9,6 @@ function ee()
 }
 
 ee pushd contract/tests/build
-ee "ctest -j \"$(nproc)\" --output-on-failure -T Test || :"
+ee "ctest -j \"$(nproc)\" --output-on-failure -T Test"
 cp "$(find ./Testing -name 'Test.xml' | sort | tail -n '1')" "../../../${XUNIT_FILENAME:-test-results.xml}"
 echo "Done! - ${0##*/}"
