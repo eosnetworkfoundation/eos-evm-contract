ab7c3167ef80db153e01e84ce7ebeb7070a7fb9a
Matias Romeo <matias.romeo@gmail.com>
2023-05-23 23:46:31 -0300

    Use default from address (0) when not specified

--- silkrpc/core/estimate_gas_oracle.cpp
+++ silkrpc/core/estimate_gas_oracle.cpp
@@ -80,6 +80,8 @@ boost::asio::awaitable<intx::uint256> EstimateGasOracle::estimate_gas(const Call
     SILKRPC_DEBUG << "hi: " << hi << ", lo: " << lo << ", cap: " << cap << "\n";
 
     silkworm::Transaction transaction{call.to_transaction()};
+    if(!transaction.from.has_value()) transaction.from = evmc::address{0};
+
     while (lo + 1 < hi) {
         auto mid = (hi + lo) / 2;
         transaction.gas_limit = mid;
