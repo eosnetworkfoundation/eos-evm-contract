d449d790eee7099d3c6cdfd5a90f158a7c2a5670
kayan <taokayan13@gmail.com>
2023-10-12 17:22:56 +0800

    fix gcc compiler error

--- tests/basic_evm_tester.hpp
+++ tests/basic_evm_tester.hpp
@@ -261,7 +261,7 @@ public:
    signed_block_ptr produce_block( fc::microseconds skip_time = fc::milliseconds(config::block_interval_ms) )override {
       auto sb = _produce_block(skip_time, false);
       auto bsf = validating_node->create_block_state_future( sb->calculate_id(), sb );
-      controller::block_report br;
+      struct controller::block_report br;
       validating_node->push_block( br, bsf.get(), forked_branch_callback{}, trx_meta_cache_lookup{} );
 
       return sb;
@@ -273,7 +273,7 @@ public:
 
    void validate_push_block(const signed_block_ptr& sb) {
       auto bsf = validating_node->create_block_state_future( sb->calculate_id(), sb );
-      controller::block_report br;
+      struct controller::block_report br;
       validating_node->push_block( br, bsf.get(), forked_branch_callback{}, trx_meta_cache_lookup{} );
    }
 
@@ -281,7 +281,7 @@ public:
       unapplied_transactions.add_aborted( control->abort_block() );
       auto sb = _produce_block(skip_time, true);
       auto bsf = validating_node->create_block_state_future( sb->calculate_id(), sb );
-      controller::block_report br;
+      struct controller::block_report br;
       validating_node->push_block( br, bsf.get(), forked_branch_callback{}, trx_meta_cache_lookup{} );
 
       return sb;
