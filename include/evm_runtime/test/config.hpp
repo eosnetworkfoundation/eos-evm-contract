2df364a7edaf3abb95bdf37a4a4179fc232ff1df
Matias Romeo <matias.romeo@gmail.com>
2024-01-19 00:01:27 -0300

    Change kTestNetwork initialization to be compatible with the new ChainConfig

--- include/evm_runtime/test/config.hpp
+++ include/evm_runtime/test/config.hpp
@@ -7,14 +7,14 @@ namespace test {
 inline constexpr ChainConfig kTestNetwork{
     .chain_id = 1,
     .protocol_rule_set = protocol::RuleSetType::kNoProof,
-    .homestead_block = 0,
-    .dao_block = 0,
-    .tangerine_whistle_block = 0,
-    .spurious_dragon_block = 0,
-    .byzantium_block = 0,
-    .constantinople_block = 0,
-    .petersburg_block = 0,
-    .istanbul_block = 0,
+    ._homestead_block = 0,
+    ._dao_block = 0,
+    ._tangerine_whistle_block = 0,
+    ._spurious_dragon_block = 0,
+    ._byzantium_block = 0,
+    ._constantinople_block = 0,
+    ._petersburg_block = 0,
+    ._istanbul_block = 0,
 };
 
 } //namespace test
